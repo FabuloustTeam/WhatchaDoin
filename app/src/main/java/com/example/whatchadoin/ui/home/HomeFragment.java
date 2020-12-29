31ffdad67e41ef13f9d01c00f76419caf51c020e
187PM13983 - Phan Nguyen Thi Yen Nhi - K24T-PM1 <nhi.187pm13983@vanlanguni.vn>
2020-12-29 21:15:27 +0700

    modify HomeFragment

--- app/src/main/java/com/example/whatchadoin/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/whatchadoin/ui/home/HomeFragment.java
@@ -2,9 +2,12 @@ package com.example.whatchadoin.ui.home;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,17 +32,33 @@ import java.util.ArrayList;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    View v;
+    View root;
     DatabaseReference reference;
     RecyclerView recyViewTasks;
-    ArrayList<Task>  listTasks;
+    ArrayList<Task> listTasks;
     TaskAdapter taskAdapter;
+
+    ListView lvContact;
+    ArrayAdapter<String> adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.v = view;
+        init();
+    }
+
+
+    public void init() {
 
         recyViewTasks = (RecyclerView) root.findViewById(R.id.recyclerViewTasks);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
@@ -50,11 +69,12 @@ public class HomeFragment extends Fragment {
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     Task taskTmp = dataSnapshot.getValue(Task.class);
                     listTasks.add(taskTmp);
                 }
-                taskAdapter = new TaskAdapter(getActivity(), listTasks);
+                recyViewTasks.setLayoutManager(new LinearLayoutManager(getContext()));
+                taskAdapter = new TaskAdapter(getContext(), listTasks);
                 recyViewTasks.setAdapter(taskAdapter);
                 taskAdapter.notifyDataSetChanged();
             }
@@ -65,6 +85,31 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        return root;
+//        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1);
+//        lvContact = root.findViewById(R.id.lvContact);
+//        lvContact.setAdapter(adapter);
+//
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("task");
+//
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                adapter.clear();
+//                for (DataSnapshot data : dataSnapshot.getChildren()) {
+//                    String key = data.getKey();
+//                    String value = data.getValue().toString();
+//                    adapter.add(key + "\n" + value);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                Log.w("Firebase", "loadPost:onCancelled", databaseError.toException());
+//            }
+//        });
     }
+
+
+
 }
